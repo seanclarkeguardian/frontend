0a0c1fefc984c25461f5a99b5628d537f2e1b217
Darren Hurley <darren.hurley@guardian.co.uk>
2013-03-14 14:27:12 +0000

    convert url field to text type (can be long)

--- db/migrate/20130313001155_create_js_error.rb
+++ db/migrate/20130313001155_create_js_error.rb
@@ -2,7 +2,7 @@ class CreateJsError < ActiveRecord::Migration
   def self.up
     create_table :js_errors do |t|
       t.timestamp :timestamp
-      t.string :url
+      t.text :url
       t.text :message
       t.text :file
       t.integer :line_no

cb5457716840dc3d841cd8b9c206e3571914454a
Darren Hurley <darren.hurley@guardian.co.uk>
2013-04-05 17:14:03 +0100

    latest story popups

--- common/app/assets/javascripts/modules/navigation/top-stories.js
+++ common/app/assets/javascripts/modules/navigation/top-stories.js
@@ -1,4 +1,4 @@
-define(['common', 'ajax', 'bonzo', 'reqwest'], function (common, ajax, bonzo, reqwest) {
+define(['common', 'ajax', 'bonzo', 'reqwest', 'bean'], function (common, ajax, bonzo, reqwest, bean) {
     
     function objectifyCookies(cookieString) {
         var cookies = {};
@@ -25,12 +25,96 @@ define(['common', 'ajax', 'bonzo', 'reqwest'], function (common, ajax, bonzo, re
                     + '<ul class="unstyled">'
                     + facets.map(function(facet) {
                         var section = facet.term;
-                        return '<li class="zone-' + section + '"><p class="type-7"><a href="/' + section + '" class="zone-color">' 
+                        return '<li class="foo zone-' + section + '"><p class="type-7"><a href="/' + section + '" class="section zone-color">' 
                             + section.substring(0, 1).toUpperCase() + section.substring(1) 
                             + '</a></p></li>';
                     }).join('')
                     + '</ul>'
                     + '</div>';
+                
+                // top stories hover
+                bean.on(topstoriesHeader, 'mouseover', 'a.section', function(e) {
+                    var link = bonzo(this),
+                        ul = bonzo(bonzo(link.parent()).next());
+                    if (ul.length !== 0) {
+                        return ul.show();
+                    }
+                    
+                    var section = link.text().toLowerCase(),
+                        dtTo = new Date().getTime();
+                    // pull in most popular story for that section (in the last minute)
+                    reqwest({
+                        url: 'http://frontend-es.ophan.co.uk:9200/_search',
+                        type: 'json',
+                        method: 'post',
+                        data: JSON.stringify({
+                            query: {
+                                bool:{
+                                    must: [
+                                        {
+                                            term: {
+                                                section: section
+                                            }
+                                        },
+                                        {
+                                            range:{
+                                                dt: {
+                                                    from: dtTo - (60 * 1000),
+                                                    to: dtTo
+                                                }
+                                            }
+                                        },
+                                        {
+                                            term: {
+                                                isContent: true
+                                            }
+                                        }
+                                    ]
+                                }
+                            },
+                            facets: {
+                                stories: {
+                                    terms: {
+                                        field : 'path',
+                                        size : 5
+                                    }
+                                }
+                            },
+                            stats: ['frontend-dashboard'],
+                            size: 0
+                        })
+                    })
+                    .then(
+                        function(resp) {
+                            console.log(resp);
+                            var results = resp.facets.stories.terms;
+                            if (results.length === 0) {
+                                return;
+                            }
+                            bonzo(link.parent()).after(
+                                '<ul style="clear:left; position:absolute; background-color:rgba(255,255,255,0.9); box-sizing:border-box; width:100%">'
+                                + results.map(function(result) {
+                                    var url = result.term,
+                                        title = url.split('/').reverse()[0].replace(/-/g, ' ');
+                                    return '<li><a href="' + url + '">' 
+                                        + title.substring(0, 1).toUpperCase() + title.substring(1)  
+                                        + '</a></li>'
+                                }).join('')
+                                + '</ul>'
+                            );
+                        }, 
+                        function(err) {
+                            console.log(err);
+                        }
+                    );
+                });
+                
+                // remove top stories
+                bean.on(topstoriesHeader, 'mouseleave', 'li.foo', function(e) {
+                    common.$g('ul', this).hide();
+                });
+                
+                bean.on(topstoriesHeader, 'mouseover', 'a.section', function(e) {})
 
                 common.mediator.emit('modules:topstories:render');
 
